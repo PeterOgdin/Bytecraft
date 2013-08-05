d641342c61a0bf6334e280a288dea725e8ea4a3b
Robert Catron <rcatron10@gmail.com>
2013-08-05 03:11:26 -0700

    Dropped items now tell you who you get the item from

--- src/info/bytecraft/listener/BytecraftPlayerListener.java
+++ src/info/bytecraft/listener/BytecraftPlayerListener.java
@@ -1,20 +1,28 @@
 package info.bytecraft.listener;
 
+import java.util.HashMap;
+
 import info.bytecraft.Bytecraft;
 import info.bytecraft.api.BytecraftPlayer;
 import info.bytecraft.api.PlayerBannedException;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerKickEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.event.player.PlayerLoginEvent.Result;
+import org.bukkit.event.player.PlayerPickupItemEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.inventory.ItemStack;
+
+import com.google.common.collect.Maps;
 
 public class BytecraftPlayerListener implements Listener
 {
@@ -51,9 +59,9 @@ public class BytecraftPlayerListener implements Listener
     @EventHandler
     public void onLogin(PlayerLoginEvent event)
     {
-        try{
+        try {
             plugin.addPlayer(event.getPlayer());
-        }catch(PlayerBannedException e){
+        } catch (PlayerBannedException e) {
             event.disallow(Result.KICK_BANNED, e.getMessage());
         }
     }
@@ -72,14 +80,42 @@ public class BytecraftPlayerListener implements Listener
         if (!(event.getEntity() instanceof Player))
             return;
         BytecraftPlayer player = plugin.getPlayer((Player) event.getEntity());
-        if (player.isAdmin()) {
-            event.setCancelled(true);
-        }
+        event.setCancelled(player.isAdmin());
     }
-    
+
     @EventHandler
     public void onKick(PlayerKickEvent event)
     {
         event.setLeaveMessage(null);
+    }
+
+    private HashMap<Item, BytecraftPlayer> droppedItems = Maps.newHashMap();
+
+    @EventHandler
+    public void onDrop(PlayerDropItemEvent event)
+    {
+        BytecraftPlayer player = plugin.getPlayer(event.getPlayer());
+        droppedItems.put(event.getItemDrop(), player);
+    }
+
+    @EventHandler
+    public void onPickup(PlayerPickupItemEvent event)
+    {
+        if (!droppedItems.containsKey(event.getItem()))
+            return;
+        BytecraftPlayer player = plugin.getPlayer(event.getPlayer());
+        BytecraftPlayer from = droppedItems.get(event.getItem());
+        ItemStack stack = event.getItem().getItemStack();
+
+        if (from != null && (from != player)) {
+            player.sendMessage(ChatColor.YELLOW + "You got " + ChatColor.GOLD
+                    + stack.getAmount() + " " + stack.getType()
+                    + ChatColor.YELLOW + " from " + from.getDisplayName() + ".");
+            from.sendMessage(ChatColor.YELLOW + "You gave "
+                    + player.getDisplayName() + ChatColor.GOLD + " "
+                    + stack.getAmount() + " "
+                    + stack.getType().name().toLowerCase().replace("_", " "));
+            droppedItems.remove(event.getItem());
+        }
     }
 }
