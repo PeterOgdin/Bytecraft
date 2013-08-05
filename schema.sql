c8e7cb95bedf6ade261005b5070d0c804902bc47
Robert Catron <rcatron10@gmail.com>
2013-08-05 03:07:41 -0700

    Updated schema.sql

--- schema.sql
+++ schema.sql
@@ -1,86 +1,100 @@
--- MySQL dump 10.13  Distrib 5.1.66, for debian-linux-gnu (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 3.5.1
+-- http://www.phpmyadmin.net
 --
--- Host: localhost    Database: tregmine1
--- ------------------------------------------------------
--- Server version	5.1.66-0+squeeze1
+-- Host: localhost
+-- Generation Time: Aug 05, 2013 at 10:05 AM
+-- Server version: 5.5.24-log
+-- PHP Version: 5.3.13
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Database: `bytecraft`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bless`
+--
+
+CREATE TABLE IF NOT EXISTS `bless` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `player_name` varchar(32) NOT NULL,
+  `x` int(255) NOT NULL,
+  `y` int(255) NOT NULL,
+  `z` int(255) NOT NULL,
+  `world` varchar(32) NOT NULL DEFAULT 'world',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `player`
 --
 
-DROP TABLE IF EXISTS `player`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `player` (
+CREATE TABLE IF NOT EXISTS `player` (
   `player_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `player_name` varchar(46) COLLATE utf8_general_ci DEFAULT NULL,
+  `player_name` varchar(46) DEFAULT NULL,
   `player_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `player_wallet` bigint(20) DEFAULT '750',
-  `player_banned` ENUM('true', 'false') NOT NULL DEFAULT 'false',
+  `player_banned` enum('true','false') NOT NULL DEFAULT 'false',
   UNIQUE KEY `uid` (`player_id`),
   KEY `player` (`player_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=44284 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `player_chatlog`
 --
 
-DROP TABLE IF EXISTS `player_chatlog`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `player_chatlog` (
+CREATE TABLE IF NOT EXISTS `player_chatlog` (
   `chatlog_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `player_id` int(10) unsigned DEFAULT NULL,
   `chatlog_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `chatlog_channel` varchar(64) CHARACTER SET latin1 DEFAULT NULL,
   `chatlog_message` varchar(255) CHARACTER SET latin1 DEFAULT NULL,
   PRIMARY KEY (`chatlog_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=61331 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `player_home`
 --
 
-DROP TABLE IF EXISTS `player_home`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `player_home` (
+CREATE TABLE IF NOT EXISTS `player_home` (
   `home_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `player_id` int(10) unsigned DEFAULT NULL,
-  `home_name` varchar(32) COLLATE utf8_general_ci DEFAULT NULL,
+  `home_name` varchar(32) DEFAULT NULL,
   `home_x` double DEFAULT NULL,
   `home_y` double DEFAULT NULL,
   `home_z` double DEFAULT NULL,
   `home_pitch` double DEFAULT NULL,
   `home_yaw` double DEFAULT NULL,
-  `home_world` varchar(32) COLLATE utf8_general_ci DEFAULT NULL,
+  `home_world` varchar(32) DEFAULT NULL,
   `home_time` double DEFAULT NULL,
   PRIMARY KEY (`home_id`),
   KEY `player_id` (`player_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=33004 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `player_orelog`
 --
 
-DROP TABLE IF EXISTS `player_orelog`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `player_orelog` (
+CREATE TABLE IF NOT EXISTS `player_orelog` (
   `orelog_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `player_id` int(10) unsigned DEFAULT NULL,
   `orelog_material` int(10) unsigned DEFAULT NULL,
@@ -91,33 +105,29 @@ CREATE TABLE `player_orelog` (
   `orelog_world` varchar(255) CHARACTER SET latin1 DEFAULT NULL,
   PRIMARY KEY (`orelog_id`),
   KEY `player_idx` (`player_id`,`orelog_timestamp`)
-) ENGINE=InnoDB AUTO_INCREMENT=9465 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `player_property`
 --
 
-DROP TABLE IF EXISTS `player_property`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `player_property` (
+CREATE TABLE IF NOT EXISTS `player_property` (
   `player_id` int(10) unsigned NOT NULL DEFAULT '0',
-  `property_key` varchar(255) COLLATE utf8_general_ci NOT NULL DEFAULT '',
-  `property_value` varchar(255) COLLATE utf8_general_ci DEFAULT NULL,
+  `property_key` varchar(255) NOT NULL DEFAULT '',
+  `property_value` varchar(255) DEFAULT NULL,
   `property_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`player_id`,`property_key`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `warps`
 --
 
-DROP TABLE IF EXISTS `warps`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `warps` (
+CREATE TABLE IF NOT EXISTS `warps` (
   `name` varchar(45) CHARACTER SET latin1 NOT NULL,
   `x` double NOT NULL,
   `y` double NOT NULL,
@@ -127,60 +137,38 @@ CREATE TABLE `warps` (
   `world` varchar(16) CHARACTER SET latin1 NOT NULL COMMENT 'added for multi world support',
   UNIQUE KEY `name.uniqe` (`name`),
   KEY `name-index` (`name`,`x`,`y`,`z`,`pitch`,`yaw`,`world`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
----
---- Table structure for table `bless`
----
-
-DROP TABLE IF EXISTS `bless`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `bless` (
-  `id` int(255) NOT NULL AUTO_INCREMENT,
-  `player_name` varchar(32) NOT NULL,
-  `x` int(255) NOT NULL,
-  `y` int(255) NOT NULL,
-  `z` int(255) NOT NULL,
-  `world` varchar(32) NOT NULL DEFAULT 'world',
-  PRIMARY KEY `id` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `zone`
 --
 
-DROP TABLE IF EXISTS `zone`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `zone` (
+CREATE TABLE IF NOT EXISTS `zone` (
   `zone_id` int(11) NOT NULL AUTO_INCREMENT,
-  `zone_world` varchar(50) COLLATE utf8_general_ci NOT NULL DEFAULT 'world',
-  `zone_name` varchar(32) COLLATE utf8_general_ci NOT NULL,
-  `zone_enterdefault` enum('0','1') COLLATE utf8_general_ci NOT NULL DEFAULT '1',
-  `zone_placedefault` enum('0','1') COLLATE utf8_general_ci NOT NULL DEFAULT '1',
-  `zone_destroydefault` enum('0','1') COLLATE utf8_general_ci NOT NULL DEFAULT '1',
-  `zone_pvp` enum('0','1') COLLATE utf8_general_ci NOT NULL DEFAULT '0',
-  `zone_hostiles` enum('0','1') COLLATE utf8_general_ci DEFAULT '1',
-  `zone_entermessage` varchar(250) COLLATE utf8_general_ci NOT NULL,
-  `zone_exitmessage` varchar(250) COLLATE utf8_general_ci NOT NULL,
-  `zone_texture` text COLLATE utf8_general_ci,
-  `zone_owner` varchar(24) COLLATE utf8_general_ci DEFAULT NULL,
+  `zone_world` varchar(50) NOT NULL DEFAULT 'world',
+  `zone_name` varchar(32) NOT NULL,
+  `zone_enterdefault` enum('0','1') NOT NULL DEFAULT '1',
+  `zone_placedefault` enum('0','1') NOT NULL DEFAULT '1',
+  `zone_destroydefault` enum('0','1') NOT NULL DEFAULT '1',
+  `zone_pvp` enum('0','1') NOT NULL DEFAULT '0',
+  `zone_hostiles` enum('0','1') DEFAULT '1',
+  `zone_entermessage` varchar(250) NOT NULL,
+  `zone_exitmessage` varchar(250) NOT NULL,
+  `zone_texture` text,
+  `zone_owner` varchar(24) DEFAULT NULL,
   PRIMARY KEY (`zone_id`),
   UNIQUE KEY `name` (`zone_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=912 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `zone_lot`
 --
 
-DROP TABLE IF EXISTS `zone_lot`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `zone_lot` (
+CREATE TABLE IF NOT EXISTS `zone_lot` (
   `lot_id` int(10) NOT NULL AUTO_INCREMENT,
   `zone_id` int(10) NOT NULL,
   `lot_name` varchar(50) NOT NULL,
@@ -190,31 +178,27 @@ CREATE TABLE `zone_lot` (
   `lot_y2` int(10) NOT NULL,
   `special` int(11) DEFAULT NULL,
   PRIMARY KEY (`lot_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11363 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `zone_lotuser`
 --
 
-DROP TABLE IF EXISTS `zone_lotuser`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `zone_lotuser` (
+CREATE TABLE IF NOT EXISTS `zone_lotuser` (
   `lot_id` int(10) NOT NULL DEFAULT '0',
   `user_id` int(10) NOT NULL DEFAULT '0',
   PRIMARY KEY (`lot_id`,`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `zone_rect`
 --
 
-DROP TABLE IF EXISTS `zone_rect`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `zone_rect` (
+CREATE TABLE IF NOT EXISTS `zone_rect` (
   `rect_id` int(10) NOT NULL AUTO_INCREMENT,
   `zone_id` int(10) DEFAULT NULL,
   `rect_x1` int(10) DEFAULT NULL,
@@ -223,31 +207,21 @@ CREATE TABLE `zone_rect` (
   `rect_y2` int(10) DEFAULT NULL,
   PRIMARY KEY (`rect_id`),
   KEY `zone_id` (`zone_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=911 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `zone_user`
 --
 
-DROP TABLE IF EXISTS `zone_user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `zone_user` (
+CREATE TABLE IF NOT EXISTS `zone_user` (
   `zone_id` int(10) unsigned NOT NULL DEFAULT '0',
   `user_id` int(10) unsigned NOT NULL DEFAULT '0',
   `user_perm` enum('owner','maker','allowed','banned') NOT NULL DEFAULT 'allowed',
   PRIMARY KEY (`zone_id`,`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2013-07-26 14:22:39
