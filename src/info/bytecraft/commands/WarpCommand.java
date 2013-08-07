413c7e27aa4161127a97c42cbae29a109fe1b603
Robert Catron <rcatron10@gmail.com>
2013-08-07 12:55:42 -0700

    Warping now adds 2 second delay

--- src/info/bytecraft/commands/WarpCommand.java
+++ src/info/bytecraft/commands/WarpCommand.java
@@ -3,6 +3,7 @@ package info.bytecraft.commands;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 
@@ -18,31 +19,52 @@ public class WarpCommand extends AbstractCommand
     {
         super(instance, "warp");
     }
-    
+
+    public class WarpTask implements Runnable
+    {
+        private BytecraftPlayer player;
+        private Location loc;
+
+        public WarpTask(BytecraftPlayer player, Location loc)
+        {
+            this.loc = loc;
+            this.player = player;
+        }
+
+        public void run()
+        {
+            player.teleport(loc);
+        }
+
+    }
+
     public boolean handlePlayer(BytecraftPlayer player, String[] args)
     {
-        if(args.length != 1)return false;
-        if(!player.isTrusted())return true;
+        if (args.length != 1)
+            return false;
+        if (!player.isTrusted())
+            return true;
         String warp = args[0];
         Connection conn = null;
-        try{
+        try {
             conn = ConnectionPool.getConnection();
             DBWarpDAO dbWarp = new DBWarpDAO(conn);
             Location loc = dbWarp.getWarp(warp, plugin.getServer());
-            if(loc != null){
-                player.teleport(loc);
-                player.sendMessage(ChatColor.AQUA + "Teleporting to " + warp);
+            if (loc != null) {
+                Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, new WarpTask(player, loc), 40L);
+                player.sendMessage(ChatColor.AQUA + "Teleporting to " + warp + " please wait");
             }
-        }catch(SQLException e){
+        } catch (SQLException e) {
             throw new RuntimeException(e);
-        }finally{
-            if(conn != null){
-                try{
+        } finally {
+            if (conn != null) {
+                try {
                     conn.close();
-                }catch(SQLException e){}
+                } catch (SQLException e) {
+                }
             }
         }
-        
+
         return true;
     }
 
