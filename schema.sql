9de3288c2d8a942a2e03692a822efb14945f0cb3
Robert Catron <rcatron10@gmail.com>
2013-08-08 23:07:50 -0700

    fix schema

--- schema.sql
+++ schema.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 08, 2013 at 11:31 PM
+-- Generation Time: Aug 09, 2013 at 03:58 AM
 -- Server version: 5.5.24-log
 -- PHP Version: 5.3.13
 
@@ -23,6 +23,57 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `bless`
+--
+
+CREATE TABLE IF NOT EXISTS `bless` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `player_name` varchar(32) NOT NULL,
+  `x` int(255) NOT NULL,
+  `y` int(255) NOT NULL,
+  `z` int(255) NOT NULL,
+  `world` varchar(32) NOT NULL DEFAULT 'world',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `fill_log`
+--
+
+CREATE TABLE IF NOT EXISTS `fill_log` (
+  `fill_id` int(255) NOT NULL AUTO_INCREMENT,
+  `player_name` varchar(255) NOT NULL,
+  `action` enum('fill','replace','undo') NOT NULL DEFAULT 'fill',
+  `size` int(255) NOT NULL,
+  `material` varchar(32) NOT NULL,
+  UNIQUE KEY `id` (`fill_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `paper_log`
+--
+DROP TABLE IF EXISTS `paper_log`;
+
+CREATE TABLE IF NOT EXISTS `paper_log` (
+  `paper_id` int(10) NOT NULL AUTO_INCREMENT,
+  `player_name` varchar(16) NOT NULL,
+  `block_x` int(32) NOT NULL,
+  `block_y` int(32) NOT NULL,
+  `block_z` int(32) NOT NULL,
+  `block_world` varchar(32) NOT NULL DEFAULT 'world',
+  `block_type` varchar(32) NOT NULL,
+  `action` enum('break','place') NOT NULL,
+  `paper_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  UNIQUE KEY `id` (`paper_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `player`
 --
 
@@ -33,10 +84,175 @@ CREATE TABLE IF NOT EXISTS `player` (
   `player_wallet` bigint(20) DEFAULT '750',
   `player_banned` enum('true','false') NOT NULL DEFAULT 'false',
   `player_rank` enum('warned','hard_warned','newcomer','settler','member','mentor','donator','gaurd','builder','admin','senior_admin') NOT NULL DEFAULT 'newcomer',
-  `player_promoted` DATE,
+  `player_promoted` date DEFAULT NULL,
   UNIQUE KEY `uid` (`player_id`),
   KEY `player` (`player_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `player_chatlog`
+--
+
+CREATE TABLE IF NOT EXISTS `player_chatlog` (
+  `chatlog_id` int(10) NOT NULL AUTO_INCREMENT,
+  `player_name` varchar(32) COLLATE utf8_bin DEFAULT NULL,
+  `chatlog_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `chatlog_channel` varchar(64) COLLATE utf8_bin DEFAULT NULL,
+  `chatlog_message` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`chatlog_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `player_home`
+--
+
+CREATE TABLE IF NOT EXISTS `player_home` (
+  `home_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `player_id` int(10) unsigned DEFAULT NULL,
+  `home_name` varchar(32) DEFAULT NULL,
+  `home_x` double DEFAULT NULL,
+  `home_y` double DEFAULT NULL,
+  `home_z` double DEFAULT NULL,
+  `home_pitch` double DEFAULT NULL,
+  `home_yaw` double DEFAULT NULL,
+  `home_world` varchar(32) DEFAULT NULL,
+  `home_time` double DEFAULT NULL,
+  PRIMARY KEY (`home_id`),
+  KEY `player_id` (`player_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `player_property`
+--
+
+CREATE TABLE IF NOT EXISTS `player_property` (
+  `player_id` int(10) unsigned NOT NULL DEFAULT '0',
+  `property_key` varchar(255) NOT NULL DEFAULT '',
+  `property_value` varchar(255) DEFAULT NULL,
+  `property_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`player_id`,`property_key`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaction_log`
+--
+
+CREATE TABLE IF NOT EXISTS `transaction_log` (
+  `transaction_id` int(32) NOT NULL AUTO_INCREMENT,
+  `sender_name` varchar(32) NOT NULL,
+  `reciever_name` varchar(32) NOT NULL,
+  `amount` bigint(20) NOT NULL,
+  UNIQUE KEY `id` (`transaction_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `warps`
+--
+
+CREATE TABLE IF NOT EXISTS `warps` (
+  `name` varchar(45) CHARACTER SET latin1 NOT NULL,
+  `x` double NOT NULL,
+  `y` double NOT NULL,
+  `z` double NOT NULL,
+  `pitch` float NOT NULL,
+  `yaw` float NOT NULL,
+  `world` varchar(16) CHARACTER SET latin1 NOT NULL,
+  UNIQUE KEY `name.uniqe` (`name`),
+  KEY `name-index` (`name`,`x`,`y`,`z`,`pitch`,`yaw`,`world`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `zone`
+--
+
+CREATE TABLE IF NOT EXISTS `zone` (
+  `zone_id` int(11) NOT NULL AUTO_INCREMENT,
+  `zone_world` varchar(50) NOT NULL DEFAULT 'world',
+  `zone_name` varchar(32) NOT NULL,
+  `zone_enterdefault` enum('0','1') NOT NULL DEFAULT '1',
+  `zone_placedefault` enum('0','1') NOT NULL DEFAULT '1',
+  `zone_destroydefault` enum('0','1') NOT NULL DEFAULT '1',
+  `zone_pvp` enum('0','1') NOT NULL DEFAULT '0',
+  `zone_hostiles` enum('0','1') DEFAULT '1',
+  `zone_entermessage` varchar(250) NOT NULL,
+  `zone_exitmessage` varchar(250) NOT NULL,
+  `zone_texture` text,
+  `zone_owner` varchar(24) DEFAULT NULL,
+  PRIMARY KEY (`zone_id`),
+  UNIQUE KEY `name` (`zone_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `zone_lot`
+--
+
+CREATE TABLE IF NOT EXISTS `zone_lot` (
+  `lot_id` int(10) NOT NULL AUTO_INCREMENT,
+  `zone_id` int(10) NOT NULL,
+  `lot_name` varchar(50) NOT NULL,
+  `lot_x1` int(10) NOT NULL,
+  `lot_y1` int(10) NOT NULL,
+  `lot_x2` int(10) NOT NULL,
+  `lot_y2` int(10) NOT NULL,
+  `special` int(11) DEFAULT NULL,
+  PRIMARY KEY (`lot_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `zone_lotuser`
+--
+
+CREATE TABLE IF NOT EXISTS `zone_lotuser` (
+  `lot_id` int(10) NOT NULL DEFAULT '0',
+  `user_id` int(10) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`lot_id`,`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `zone_rect`
+--
+
+CREATE TABLE IF NOT EXISTS `zone_rect` (
+  `rect_id` int(10) NOT NULL AUTO_INCREMENT,
+  `zone_id` int(10) DEFAULT NULL,
+  `rect_x1` int(10) DEFAULT NULL,
+  `rect_y1` int(10) DEFAULT NULL,
+  `rect_x2` int(10) DEFAULT NULL,
+  `rect_y2` int(10) DEFAULT NULL,
+  PRIMARY KEY (`rect_id`),
+  KEY `zone_id` (`zone_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `zone_user`
+--
+
+CREATE TABLE IF NOT EXISTS `zone_user` (
+  `zone_id` int(10) unsigned NOT NULL DEFAULT '0',
+  `user_id` int(10) unsigned NOT NULL DEFAULT '0',
+  `user_perm` enum('owner','maker','allowed','banned') NOT NULL DEFAULT 'allowed',
+  PRIMARY KEY (`zone_id`,`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
