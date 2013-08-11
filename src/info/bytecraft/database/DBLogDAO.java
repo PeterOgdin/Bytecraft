4c76a986e7176989bec4dddac2734c04b92b29c3
Robert Catron <rcatron10@gmail.com>
2013-08-10 20:07:55 -0700

    Misc changes

--- src/info/bytecraft/database/DBLogDAO.java
+++ src/info/bytecraft/database/DBLogDAO.java
@@ -1,21 +1,29 @@
 package info.bytecraft.database;
 
 import info.bytecraft.api.BytecraftPlayer;
+import info.bytecraft.api.PaperLog;
 import info.bytecraft.blockfill.Fill;
 import info.bytecraft.blockfill.Fill.Action;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.Collections;
+import java.util.List;
 
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 
+import com.google.common.collect.Lists;
+
 public class DBLogDAO
 {
 
     private Connection conn;
+    private final SimpleDateFormat format = new SimpleDateFormat("MM/dd/YY hh:mm:ss a");
 
     public DBLogDAO(Connection conn)
     {
@@ -96,8 +104,8 @@ public class DBLogDAO
     {
         PreparedStatement stm = null;
         try{
-            stm = conn.prepareStatement("INSERT INTO paper_log (player_name, block_x, block_y, block_z, block_type, block_world, action) " +
-            		"VALUES (?, ?, ?, ?, ?, ?, ?)");
+            stm = conn.prepareStatement("INSERT INTO paper_log (player_name, block_x, block_y, block_z, block_type, paper_time, block_world, action) " +
+            		"VALUES (?, ?, ?, ?, ?, unix_timestamp(), ?, ?)");
             stm.setString(1, player.getName());
             stm.setInt(2, loc.getBlockX());
             stm.setInt(3, loc.getBlockY());
@@ -128,12 +136,40 @@ public class DBLogDAO
             stm.setInt(2, block.getY());
             stm.setInt(3, block.getZ());
             stm.setString(4, block.getWorld().getName());
-            
             stm.execute();
-            if(stm.getResultSet().next()){
-                return false;
-            }else{
-                return true;
+            ResultSet rs = stm.getResultSet();
+            return !rs.next();
+        }catch(SQLException e){
+            throw new RuntimeException(e);
+        }finally{
+            if(stm != null){
+                try {
+                    stm.close();
+                } catch (SQLException e) {}
+            }
+        }
+    }
+    
+    public List<PaperLog> getLogs(Block block)
+    {
+        List<PaperLog> logs = Lists.newArrayList();
+        PreparedStatement stm = null;
+        try{
+            stm = conn.prepareStatement("SELECT * FROM paper_log WHERE block_x = ? AND block_y = ? AND block_z = ? AND block_world = ? " +
+            		"ORDER BY paper_time DESC LIMIT 5");
+            stm.setInt(1, block.getX());
+            stm.setInt(2, block.getY());
+            stm.setInt(3, block.getZ());
+            stm.setString(4, block.getWorld().getName());
+            stm.execute();
+            ResultSet rs = stm.getResultSet();
+            while(rs.next()){
+                String player = rs.getString("player_name");
+                java.util.Date date = new java.util.Date(rs.getInt("paper_time") * 1000L);
+                String action = rs.getString("action");
+                String material = rs.getString("block_type");
+                PaperLog log = new PaperLog(player, format.format(date), action, material);
+                logs.add(log);
             }
         }catch(SQLException e){
             throw new RuntimeException(e);
@@ -144,5 +180,7 @@ public class DBLogDAO
                 } catch (SQLException e) {}
             }
         }
+        Collections.reverse(logs);
+        return logs;
     }
 }
