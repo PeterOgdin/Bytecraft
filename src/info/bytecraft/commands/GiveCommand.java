bcb5d995f33909333f5503a6674d7b2e51c70869
Sabersamus <rcatron10@gmail.com>
2013-08-30 12:51:11 -0700

    give works from console

--- src/info/bytecraft/commands/GiveCommand.java
+++ src/info/bytecraft/commands/GiveCommand.java
@@ -7,6 +7,7 @@ import static org.bukkit.ChatColor.*;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.Server;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
@@ -21,12 +22,8 @@ public class GiveCommand extends AbstractCommand
     @Override
     public boolean handlePlayer(BytecraftPlayer player, String[] args)
     {
-        if (args.length == 0) {
-            return false;
-        }
-        if (!player.isAdmin()) {
-            return false;
-        }
+        if (args.length == 0)return true;
+        if (!player.isAdmin())return true;
 
         String param = args[1].toUpperCase();
         Player delegate = Bukkit.getPlayer(args[0]);
@@ -78,6 +75,63 @@ public class GiveCommand extends AbstractCommand
             String materialName = material.toString();
 
             player.sendMessage("You gave " + amount + " of " + DARK_AQUA
+                    + materialName.toLowerCase() + " to " + target.getName()
+                    + ".");
+            target.sendMessage(YELLOW
+                    + "You were gifted by the gods. Look in your "
+                    + "inventory!");
+
+        }
+        return true;
+    }
+    
+    public boolean handleOther(Server server, String[] args)
+    {
+        if (args.length == 0)return true;
+        String param = args[1].toUpperCase();
+        Player delegate = Bukkit.getPlayer(args[0]);
+        if (delegate != null) {
+            BytecraftPlayer target = plugin.getPlayer(delegate);
+
+            int materialId = 0;
+            try {
+                materialId = Integer.parseInt(param);
+            } catch (NumberFormatException e) {
+                try {
+                    Material material = Material.getMaterial(param);
+                    materialId = material.getId();
+                } catch (NullPointerException ne) { return true; }
+            }
+
+            int amount;
+            try {
+                amount = Integer.parseInt(args[2]);
+            } catch (ArrayIndexOutOfBoundsException e) {
+                amount = 1;
+            } catch (NumberFormatException e) {
+                amount = 1;
+            }
+
+            int data;
+            try {
+                data = Integer.parseInt(args[3]);
+            } catch (ArrayIndexOutOfBoundsException e) {
+                data = 0;
+            } catch (NumberFormatException e) {
+                data = 0;
+            }
+
+            ItemStack item = new ItemStack(materialId, amount, (byte) data);
+            if (item.getType() == Material.MONSTER_EGG
+                    || item.getType() == Material.NAME_TAG)return true;
+
+            PlayerInventory inv = target.getInventory();
+            inv.addItem(item);
+
+            Material material = Material.getMaterial(materialId);
+            String materialName = material.toString();
+
+            plugin.getLogger().info("You gave " + amount + " of " + DARK_AQUA
                     + materialName.toLowerCase() + " to " + target.getName()
                     + ".");
             target.sendMessage(YELLOW
